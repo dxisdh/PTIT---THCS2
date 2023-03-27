1541681c2d59d81fa502fb19d8c18699252358f4
Thanh Binh <70571944+dxisdh@users.noreply.github.com>
2023-03-27 16:18:53 +0700

    Create C02019 - TAM GIÁC SỐ - 5.c

---
+++ C02019 - TAM GIÁC SỐ - 5.c
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+#include<math.h> 
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = 1; j <= i; j++)
+		{
+			printf("%d", 2 * j);
+		}
+		for(int j = i - 1; j >= 1; j--)
+		{
+			printf("%d", 2 * j);
+		}
+		printf("\n");
+	}
+}
