22cafe66f3e24ed274715a1dd79254305538eccd
Thanh Binh <70571944+dxisdh@users.noreply.github.com>
2023-03-27 08:18:42 +0700

    Create C02031 - TAM GIÁC KÝ TỰ - 5.c

---
+++ C02031 - TAM GIÁC KÝ TỰ - 5.c
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+#include<math.h>
+int main()
+{ 
+	int n;
+	scanf("%d", &n);
+	for(int i = n; i >= 1; i--)
+	{
+		int k = i - 1;
+		for(int j = 1; j <= i; j++)
+		{
+			if(i == 1)
+			printf("@");
+			else
+			printf("%c", 64 + k);
+			k++;
+		}
+		printf("\n");
+	}
+}
