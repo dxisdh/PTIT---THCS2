d35717c0450143f640874ae9e947d63baa871c1a
Thanh Binh <70571944+dxisdh@users.noreply.github.com>
2023-03-27 08:46:04 +0700

    Create C04014 - ĐẾM SỐ LẦN XUẤT HIỆN 1.c

---
+++ C04014 - ĐẾM SỐ LẦN XUẤT HIỆN 1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int a[100];
+	int dem = 0;
+	for(int i = 0; i < n; i++)
+	scanf("%d", &a[i]);
+	int b[100] = {0};
+	for(int i = 0; i < n; i++)
+	b[a[i]]++;
+	for(int i = 0; i < n; i++)
+	{
+		if(b[a[i]] >= 1)
+		{
+			printf("%d %d\n", a[i], b[a[i]]);
+			b[a[i]] = 0;
+		}
+	}
+}			
