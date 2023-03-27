f1db014a0efba3077c8bb0d755de64b3cc0b4e3c
Thanh Binh <70571944+dxisdh@users.noreply.github.com>
2023-03-27 08:21:13 +0700

    Create C03004 - ƯỚC SỐ CHUNG LỚN NHẤT – BỘI SỐ CHUNG NHỎ NHẤT.c

---
+++ C03004 - ƯỚC SỐ CHUNG LỚN NHẤT – BỘI SỐ CHUNG NHỎ NHẤT.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+
+long long UCLN(long long a, long long b)
+{
+	if(a == 0 || b == 0)
+	return a + b;
+	while(a != b)
+	{
+		if(a > b)
+		a -= b;
+		else
+		b -= a;
+	}
+	return a;
+}
+
+long long BCNN(long long a, long long b)
+{
+	long long result = UCLN(a, b);
+	return a * b / result;
+}
+
+int main()
+{
+	long long a, b;
+	scanf("%lld%lld", &a, &b);
+	printf("%lld\n%lld\n", UCLN(a, b), BCNN(a, b));
+}
