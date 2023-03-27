a7c71d9f5ec41b459a80ff6a432bdfc873139054
Thanh Binh <70571944+dxisdh@users.noreply.github.com>
2023-03-27 16:21:00 +0700

    Create C02029 - TAM GIÁC KÝ TỰ - 3.c

---
+++ C02029 - TAM GIÁC KÝ TỰ - 3.c
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+#include<math.h> 
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	for(int i = 1; i <= n; i++)
+	{
+		int k = i;
+		int m = n - 1;
+		for(int j = 1; j <= i; j++)
+		{
+			printf("%c ", 64 + k);
+			k += m;
+			--m;
+		}
+		printf("\n");
+	}
+}
