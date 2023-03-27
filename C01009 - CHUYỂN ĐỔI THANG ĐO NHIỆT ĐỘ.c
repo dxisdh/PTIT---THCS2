79ac211021ab029d72c33b84447f64f3781e7e25
Thanh Binh <70571944+dxisdh@users.noreply.github.com>
2023-03-27 07:37:56 +0700

    Update and rename draft.c to C01009 - CHUYỂN ĐỔI THANG ĐO NHIỆT ĐỘ.c

---
+++ C01009 - CHUYỂN ĐỔI THANG ĐO NHIỆT ĐỘ.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	printf("%.2lf", ((double)a * 9 / 5) + 32);
+}
--- draft.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-
-int main()
-{
-	long long a, b;
-	scanf("%lld%lld", &a, &b);
-	printf("%lld\n%lld\n%lld\n%lld\n%lld\n%.2f\n", a + b, a - b, a * b, a / b, a % b, (float)a / b);
-}
