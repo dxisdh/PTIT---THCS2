21cb06deadc9d1afb283dc1df2ce0f16c06d886c
Thanh Binh <70571944+dxisdh@users.noreply.github.com>
2023-03-27 08:26:46 +0700

    Create C03029 - SỐ CHẴN ĐẶC BIỆT.c

---
+++ C03029 - SỐ CHẴN ĐẶC BIỆT.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+
+int check(long long n)
+{
+	while(n > 0)
+	{
+		long long k = n % 10;
+		if(k % 2 != 0)
+		return 0;
+		n /= 10;
+	}
+	return 1;
+}
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	while(t--)
+	{
+		long long n;
+		scanf("%lld", &n);
+		if(check(n) == 1)
+		printf("YES\n");
+		else
+		printf("NO\n");
+	}
+}
