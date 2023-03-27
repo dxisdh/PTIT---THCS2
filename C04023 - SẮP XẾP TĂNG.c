e697cc8901b3032bbab836f0ba629056a2ec7cef
Thanh Binh <70571944+dxisdh@users.noreply.github.com>
2023-03-27 15:58:04 +0700

    Create C04023 - SẮP XẾP TĂNG.c

---
+++ C04023 - SẮP XẾP TĂNG.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int a[1000];
+	for(int i = 0; i < n; i++)
+	scanf("%d", &a[i]);
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = i + 1; j < n; j++)
+		{
+			if(a[i] > a[j])
+			{
+				int tg = a[i];
+				a[i] = a[j];
+				a[j] = tg;
+			}
+		}
+	}
+	for(int i = 0; i < n; i++)
+	printf("%d ", a[i]);
+}
