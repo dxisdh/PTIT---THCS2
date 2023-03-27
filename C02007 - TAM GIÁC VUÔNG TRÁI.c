5a3e059fd327df147d5d2792dbf953b4064abd21
Thanh Binh <70571944+dxisdh@users.noreply.github.com>
2023-03-27 08:08:26 +0700

    Create C02007 - TAM GIÁC VUÔNG TRÁI.c

---
+++ C02007 - TAM GIÁC VUÔNG TRÁI.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+
+int main() 
+{ 
+	int n, m; 
+	scanf("%d", &n); 
+	for(int i = 1; i <= n; i++)
+	{ 
+		for(int j = 1; j <= i; j++)
+		{ 
+			printf("*"); 
+		} 
+		printf("\n\n"); 
+	} 
+	return 0; 
+}
